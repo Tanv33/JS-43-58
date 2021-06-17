e3a734c7861db64ab2e54a9b591ca5fab3415fe9
M.Tanveer Khan <tanveer.khan2692000@gmail.com>
2021-06-17 19:41:44 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,163 @@
+// ***************************************Chapter 43-48**********************************
+
+// function del() {
+//     var a = document.getElementById('del1')
+//     a.replaceWith()
+//     console.log(a)
+// }
+
+// function dela() {
+//     var a = document.getElementById('del2')
+//     a.replaceWith()
+//     console.log(a)
+// }
+
+// function delb() {
+//     var a = document.getElementById('del3')
+//     a.replaceWith()
+//     console.log(a)
+// }
+
+// function delc() {
+//     var a = document.getElementById('del4')
+//     a.replaceWith()
+//     console.log(a)
+// }
+
+// function deld() {
+//     var a = document.getElementById('del5')
+//     a.replaceWith()
+//     console.log(a)
+// }
+
+// function dele() {
+//     var a = document.getElementById('del6')
+//     a.replaceWith()
+//     console.log(a)
+// }
+
+// function delf() {
+//     var a = document.getElementById('del7')
+//     a.replaceWith()
+//     console.log(a)
+// }
+
+// function delg() {
+//     var a = document.getElementById('del8')
+//     a.replaceWith()
+//     console.log(a)
+// }
+
+// function delh() {
+//     var a = document.getElementById('del9')
+//     a.replaceWith()
+//     console.log(a)
+// }
+
+// function deli() {
+//     var a = document.getElementById('del10')
+//     a.replaceWith()
+//     console.log(a)
+// }
+
+
+
+
+
+// var click = document.getElementById("changeVal").innerHTML;
+
+// function increase() {
+//     click++;
+//     document.getElementById("changeVal").innerHTML = click
+// }
+
+// function decrease() {
+//     click--;
+//     document.getElementById("changeVal").innerHTML = click
+// }
+
+
+
+
+
+//  ***************************************Chapter 49-52**********************************
+
+// function forum1() {
+//     var a = document.getElementById("result1").value
+//     var b = document.getElementById("result2").value
+//     var c = document.getElementById("result3").value
+//     var d = document.getElementById("result4").value
+//     var e = document.getElementById("result5").value
+//     document.write(a + "<br>" + b + "<br>" + c + "<br>" + d + "<br>" + e + "<br>")
+// }
+
+
+
+// function lengthText() {
+//     var a = document.getElementById("fullPg")
+//     var b = "In computer science, heapsort is a comparison-based sorting algorithm. Heapsort can be thought of as an improved selection sort: like selection sort, heapsort divides its input into a sorted and an unsorted region, and it iteratively shrinks the unsorted region by extracting the largest element from it and inserting it into the sorted region. Unlike selection sort, heapsort does not waste time with a linear-time scan of the unsorted region; rather, heap sort maintains the unsorted region in a heap data structure to more quickly find the largest element in each step.[1]Although somewhat slower in practice on most machines than a well-implemented quicksort, it has the advantage of a more favorable worst-case O(n log n) runtime. Heapsort is an in-place algorithm, but it is not a stable sort.Heapsort was invented by J. W. J. Williams in 1964.[2] This was also the birth of the heap, presented already by Williams as a useful data structure in its own right.[3] In the same year, R. W. Floyd published an improved version that could sort an array in-place, continuing his earlier research into the treesort algorithm.[3]"
+//     a.innerHTML = b;
+// }
+
+// function dele1() {
+//     var a = document.getElementById('row1')
+//     a.replaceWith()
+// }
+
+// function dele2() {
+//     var a = document.getElementById('row2')
+//     a.replaceWith()
+// }
+
+// function dele3() {
+//     var a = document.getElementById('row3')
+//     a.replaceWith()
+// }
+
+// function dele4() {
+//     var a = document.getElementById('row4')
+//     a.replaceWith()
+// }
+
+// function dele5() {
+//     var a = document.getElementById('row5')
+//     a.replaceWith()
+// }
+
+// function dele6() {
+//     var a = document.getElementById('row6')
+//     a.replaceWith()
+// }
+
+// function dele7() {
+//     var a = document.getElementById('row7')
+//     a.replaceWith()
+// }
+
+// function dele8() {
+//     var a = document.getElementById('row8')
+//     a.replaceWith()
+// }
+
+// function dele9() {
+//     var a = document.getElementById('row9')
+//     a.replaceWith()
+// }
+
+// function dele10() {
+//     var a = document.getElementById('row10')
+//     a.replaceWith()
+// }
+
+// function edit() {
+//     var editInp = document.createElement('input');
+//     editInp.setAttribute('type', 'text')
+//     editInp.setAttribute('id', 'toSend')
+//     var save = document.createElement('button')
+//     var save1 = document.createTextNode('Save')
+//     save.appendChild(save1)
+//     a.parentNode.appendChild(editInp)
+//     a.parentNode.appendChild(save)
+//     save.setAttribute('onclick', 'saving(this)')
+//     document.getElementById('dis').disabled = true;
+// }
